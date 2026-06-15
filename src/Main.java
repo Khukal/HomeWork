c769a38a8e383d93e9cb192f7fc8faee0bcc6a52
khukal <en_gne@mail.ru>
2026-06-15 08:12:05 +0300

    изменения

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,51 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("привет мир !"));
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 1; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
+
+var dog=8.0;
+var cat=3.6;
+var paper=763789;
+        System.out.println (dog+"   "+cat+"   "+paper);
+         dog=dog+4;
+         cat=cat+4;
+         paper=paper+4;
+        System.out.println (dog+"   "+cat+"   "+paper);
+        dog=dog-3.5;
+        cat=cat-1.6;
+        paper=paper-7639;
+        System.out.println (dog+"   "+cat+"   "+paper);
+var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+
+       var weight1=78.2;
+        var weight2=82.7;
+        var massa=weight1+weight2;
+        System.out.println ("общий вес бойцов: "+massa);
+
+        var ostatok=weight2%weight1;
+        System.out.println("остаток от деления масс :"+ostatok);
+        var hours=640;
+        var hoursPer=8;
+        var people= hours/hoursPer;
+        System.out.println("Всего работников в компании — " + people + " человек");
+        people=people+94;
+        var workTime=people*8;
+        System.out.println("если в компании работает "+people+" человек, то всего "+workTime+" часов работы может быть поделено между сотрудниками");
     }
 }
